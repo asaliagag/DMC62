9dcdb0393947d46ed130a745777fed59ae41ce64
asaliagag <aliagag.andrea@gmail.com>
2026-09-03 22:18:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,16 @@ st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Andrea Aliaga")
 
-modulos = st.selectbox("Selecione el módulo",["Listas", "Arreglos", "Funciones", "POO"])
+modulos = st.sidebar.selectbox("Selecione el módulo",["Listas", "Arreglos", "Funciones", "POO"])
+
+if modulos == "Listas":
+  st.write("Te encuentras en el módulo de listas")
+
+elif modulos == "Arreglos":
+  st.write("Te encuentras en el módulo de arreglos")
+
+elif modulos == "Funciones":
+  st.write("Te encuentras en el módulo de Funciones")
+  
+else:
+  st.write("Te encuentras en el módulo de POO")
