4a952461fd79a08c68da64c75e5c4d06599ec034
asaliagag <aliagag.andrea@gmail.com>
2026-09-03 22:11:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
 import streamlit as st
+
+st.tittle("Especialización Python for analytics")
+st.sidebar.title("Parámetros")
+st.write("Elaborado por: Andrea Aliaga")
+
+modulos = st.selectbox("Selecione el módulo",["Listas", "Arreglos", "Funciones", "POO"])
