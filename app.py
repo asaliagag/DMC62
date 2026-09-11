fc9b0dccf6df77bc0e590efed48378cf03dfcf31
asaliagag <aliagag.andrea@gmail.com>
2026-09-10 22:34:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import libreria_funciones as lf
 
 st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
-st.write("Elaborado por: Andrea aliaga")
+st.write("Elaborado por: Andrea Aliaga")
 
 st.image("Python_logo.png",width =300)
 st.sidebar.image("DMC.png")
@@ -33,13 +33,15 @@ elif modulos == "Arreglos":
 
 elif modulos == "Funciones":
   st.write("Te encuentras en el módulo de Funciones")
-  capital_i= st.number_input("Ingrese el capital inicial",min_value = 0,max_value = 100000, value=1000)
-  sporte_m= st.number_input("Ingrese el aporte mensual",min_value = 0,max_value = 10000, value=100)
-  tasa_a= st.slider("Ingrese la tasa",min_value = 0.01,max_value = 1.00, value=0.05)
-  anios= st.slider("Ingrese los años",min_value = 1,max_value = 20, value=2)
+  capital_i = st.number_input("Ingrese el capital inicial", min_value = 0 , max_value = 100000, value=1000)
+  aporte_m = st.number_input("Ingrese el aporte mensual", min_value = 0 , max_value = 10000, value=100)
+  tasa_a = st.slider("Ingrese el aporte mensual", min_value = 0.01 , max_value = 1.00, value=0.05)
+  anios = st.slider("Ingrese el aporte mensual", min_value = 1 , max_value = 20, value=2)
 
   resultado_valor_futuro = lf.valor_futuro_inversion(capital_i ,aporte_m,tasa_a,anios)
   st.write("El resultados de tu valor futuro de inversión es: ",round(resultado_valor_futuro,2))
-
+  
 else:
   st.write("Te encuentras en el módulo de POO")
+  
+
