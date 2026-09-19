5379c0097d7661cb477fcd00c89eebdd4a387cfd
asaliagag <aliagag.andrea@gmail.com>
2026-09-19 15:17:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -50,12 +50,13 @@ if opcion == "Home":
 
     st.subheader("Información del estudiante")
 
-    st.write("**Nombre:** ANDREA ALIAGA GARMA")
+    st.write("**Nombre:** [COMPLETAR NOMBRE]")
     st.write("**Curso:** Python for Analytics")
-    st.write("**Módulo:** Módulo 1 - Python Fundamentals")
+    st.write("**Módulo:** Módulo 1")
     st.write("**Año:** 2026")
 
     st.markdown("---")
+
     st.subheader("Descripción del proyecto")
 
     st.markdown("""
@@ -69,15 +70,16 @@ if opcion == "Home":
     - Registrar ventas de productos.
     - Analizar el margen neto del negocio.
     - Gestionar el inventario de productos.
+
     """)
 
     st.subheader("Tecnologías utilizadas")
-    
-        st.write("🐍 Python")
-        st.write("📊 Streamlit")
-        st.write("🐼 Pandas")
-        st.write("🔢 NumPy")
-        st.write("📦 Programación Orientada a Objetos")
+
+    st.write("🐍 Python")
+    st.write("📊 Streamlit")
+    st.write("🐼 Pandas")
+    st.write("🔢 NumPy")
+    st.write("📦 Programación Orientada a Objetos")
 
 # EJERCICIO 1
 # FLUJO DE CAJA CON LISTAS
